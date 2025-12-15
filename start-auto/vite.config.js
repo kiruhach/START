e7f83dcdd5778ecf9caf9ee3f8d01c10a97c66c6
kiruhach <pochta23891@inbox.ru>
2025-12-15 14:31:29 +1000

    START

--- start-auto/vite.config.js
+++ start-auto/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        yandex: resolve(__dirname, 'public/yandex_3bd5675eaf36de32.html')
+      }
+    }
+  },
+  publicDir: 'public'
 })
