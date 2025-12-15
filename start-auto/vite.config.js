502884bcdc059028acf141102d68b151f9a26c12
kiruhach <pochta23891@inbox.ru>
2025-12-15 14:37:51 +1000

    Add files via upload

--- start-auto/vite.config.js
+++ start-auto/vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/', // или './' для относительных путей
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        yandex: resolve(__dirname, 'public/yandex_3bd5675eaf36de32.html')
-      }
-    }
-  },
-  publicDir: 'public'
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
 })
